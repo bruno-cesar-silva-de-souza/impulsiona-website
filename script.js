125b230b25c445e6fb87ca0ef5b93ac8d7f74efb
Bruno Souza <90866542+bruno-cesar-silva-de-souza@users.noreply.github.com>
2024-10-22 21:39:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Website de aeromodelos carregado.");
 });
+
+
+
+// // Get the contact button and info elements
+// const contactButton = document.getElementById('contact-button');
+// const contactInfo = document.getElementById('contact-info');
+
+// // Add an event listener to the contact button
+// contactButton.addEventListener('click', () => {
+//   // Toggle the display of the contact info
+//   contactInfo.style.display = contactInfo.style.display === 'none' ? 'block' : 'none';
+// });
